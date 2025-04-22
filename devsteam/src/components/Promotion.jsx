231fd67852b2f0d05c871fea53139073c7035a18
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-04-22 11:26:54 -0300

    Ajustes no PromoCard

--- devsteam/src/components/Promotion.jsx
+++ devsteam/src/components/Promotion.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import PromoCard from './PromoCard'
 
 const Promotion = () => {
   return (
     <div id='promotion'>
         <h2>Promoções</h2>
         <div id='itensPromo'>
-            
+           {/* iniciado os cards de promoção  */}
+           <PromoCard />
         </div>
     </div>
   )
