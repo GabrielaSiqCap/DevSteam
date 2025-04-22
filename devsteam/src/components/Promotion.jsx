e9fe8db37c00ad57db959f3a29c91a558fa815fb
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-04-22 14:17:09 -0300

    PromoCard mockup finalizado

--- devsteam/src/components/Promotion.jsx
+++ devsteam/src/components/Promotion.jsx
@@ -5,9 +5,11 @@ const Promotion = () => {
   return (
     <div id='promotion'>
         <h2>Promoções</h2>
-        <div id='itensPromo'>
+        <div id='itensPromo' className='d-flex flex-wrap gap-4 justify-content-around'>
            {/* iniciado os cards de promoção  */}
-           <PromoCard />
+     <PromoCard />
+     <PromoCard />
+     <PromoCard />
         </div>
     </div>
   )
