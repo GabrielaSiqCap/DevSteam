2a7cfbe76bb355c7674f4d97a1a887a2a5200287
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-04-22 16:57:59 -0300

    Adicionando carrinho

--- devsteam/src/App.jsx
+++ devsteam/src/App.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Promotion from './components/Promotion'
 
 function App() {
   
+const[contadorJogos, setContadorJogos] = useState([]);
+const[mostraCarrinho, setMostraCarrinho] = useState(false);
+
+const adicionarCarrinho = (produto) => {
+setCarrinhoItem((produtosAnteriores)=>{
+  const existing = produtosAnteriores.find(
+    (item) => item.id === produto.id
+  );
+  if (existing) {
+    return produtosAnteriores.map((item) =>
+      item.id === produto.id ? { ...item, quantidade: item.quantidade + 1 } : item
+    );
+  }else {
+    return [...produtosAnteriores, { ...produto, quantidade: 1 }];
+  }
+})
+}
 
   return (
     <>
 
-     <Header />
-     <Promotion />
+     <Header contadorJogos={2}/>
+     <Promotion adicionarCarrinho={adicionarCarrinho}/>
     </>
   )
 }
